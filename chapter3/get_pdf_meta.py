827984eb6e29cfa306c7c554ad79bf8558ca31fe
embpgp <522943492@qq.com>
2017-01-15 22:03:41 +0800

    get_pdf_meta_data

---
+++ chapter3/get_pdf_meta.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:UTF-8 -*-
+#
+#Filename:get_pdf_meta.py
+#
+#Description:get meta data from pef file
+#
+#Author:rutk1t0r
+#
+#Date:2017.01.15
+#
+#GPL
+#
+# wget http://www.wired.com/images_blogs/threatlevel/2010/12/ANONOPS_The_Press_Release.pdf
+
+
+import pyPdf
+from pyPdf import PdfFileReader
+import optparse
+
+def printMeta(fileName):
+	pdfFile = PdfFileReader(file(fileName, 'rb'))
+	docInfo = pdfFile.getDocumentInfo()
+	print('[*] PDF MetaData For:' + str(fileName))
+	for metaItem in docInfo:
+		print('[+]' + metaItem + ':' + docInfo[metaItem])
+
+
+def main():
+	parser = optparse.OptionParser('usage %parg -F<PDF file name>')
+	parser.add_option('-F', dest='fileName', type='string',help='specify PDF file name')
+	(options, args) = parser.parse_args()
+	fileName = options.fileName
+	if fileName == None:
+		print(parser.usage)
+		exit(1)
+	else:
+		printMeta(fileName)
+
+if __name__ == '__main__':
+	main()
